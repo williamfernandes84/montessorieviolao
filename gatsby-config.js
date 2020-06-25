743f247d3e638d400bd1e610a8ba3af08eb09152
System Administrator <root@mac-mini.local>
2020-06-25 12:30:49 +0200

    update files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,16 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    locale: "en",
+    title: `Montessori e Violão`,
+    description: `Ajudamos famílias na alfabetização e desenvolvimento de suas crianças`,
+    author: `@montessorieviolaon`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +18,72 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.API_URL || "http://161.35.100.47",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "article",
+          "category",
+          "lp",
+          "paginas",
+          "confirmacao",
+          "configuracao",
+          "link"
+        ],
+        queryLimit: 1000,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint: 'https://montessorieviolao.us17.list-manage.com/subscribe/post?u=9d05439abee4d08e9a26252a9&amp;id=3a1d3ba267',
+        timeout: 3500,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-N5RW2WX",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        //dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "gatsby-route-change",
+      },
+    },
+
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,125 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(
+    `
+      {
+        articles: allStrapiArticle {
+          edges {
+            node {
+              strapiId
+            }
+          }
+        }
+        categories: allStrapiCategory {
+          edges {
+            node {
+              strapiId
+            }
+          }
+        }
+        landingpages: allStrapiLp {
+          edges {
+            node {
+              strapiId
+              titulo
+            }
+          }
+        }
+        paginas: allStrapiPaginas {
+          edges {
+            node {
+              strapiId
+            }
+          }
+        }
+        confirmacoes: allStrapiConfirmacao {
+          edges {
+            node {
+              strapiId
+              titulo
+            }
+          }
+        }
+        links: allStrapiLink {
+          edges {
+            node {
+              strapiId
+            }
+          }
+        }
 
-// You can delete this file if you're not using it
+
+
+      }
+    `
+  )
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  // Create blog articles pages.
+  const articles = result.data.articles.edges
+  const categories = result.data.categories.edges
+  const paginas = result.data.paginas.edges
+  const landingpages = result.data.landingpages.edges
+  const confirmacoes = result.data.confirmacoes.edges
+  const links = result.data.links.edges
+
+  createPage({
+      path: `/links`,
+      component: require.resolve("./src/templates/links.js"),
+    })
+
+  confirmacoes.forEach((confirmacao, index) => {
+    createPage({
+      path: `/confirmacao/${confirmacao.node.titulo}`,
+      component: require.resolve("./src/templates/confirmacao.js"),
+      context: {
+        id: confirmacao.node.strapiId,
+      },
+    })
+  })
+
+  articles.forEach((article, index) => {
+    createPage({
+      path: `/article/${article.node.strapiId}`,
+      component: require.resolve("./src/templates/article.js"),
+      context: {
+        id: article.node.strapiId,
+      },
+    })
+  })
+
+  categories.forEach((category, index) => {
+    createPage({
+      path: `/category/${category.node.strapiId}`,
+      component: require.resolve("./src/templates/category.js"),
+      context: {
+        id: category.node.strapiId,
+      },
+    })
+  })
+
+  paginas.forEach((pagina, index) => {
+    createPage({
+      path: `/pagina/${pagina.node.strapiId}`,
+      component: require.resolve("./src/templates/pagina.js"),
+      context: {
+        id: pagina.node.strapiId,
+      },
+    })
+  })
+
+  landingpages.forEach((landingpage, index) => {
+    createPage({
+      path: `/lp/${landingpage.node.titulo}`,
+      component: require.resolve("./src/templates/lp.js"),
+      context: {
+        id: landingpage.node.strapiId,
+      },
+    })
+  })
+
+
+}
