73a6b6ad1a5d8162b769aafc82ffee4af3402782
System Administrator <root@mac-mini.local>
2020-06-25 14:22:00 +0200

    update files

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
+import HomeIcon from '@material-ui/icons/Home';
 
 const Logo = () => (
   <div>
@@ -30,7 +31,7 @@ const Logo = () => (
                     <div className="uk-navbar-left">
                     <ul className="uk-navbar-nav">
                       <li>
-                        <Link to={`/`}> <img className="logo-nav" src={config.node.image.publicURL} alt="LOGO" /></Link>
+                        <Link to={`/`}> <HomeIcon /></Link>
                       </li>
 
                     </ul>
